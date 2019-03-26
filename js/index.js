e7039163ee46e7b7e02b1178bd2321aadc88d043
praneethkumar900 <praneethkumar900@gmail.com>
2019-03-26 18:16:29 -0500

    Index and homepage modifications
    Added animations, enhanced details.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
-$("#register_first").click(function(){
-  $(".login").hide(1000);
-  $(".register").show(1500);
+
+$("#register_link").click(function(){
+  $(".login").removeClass("flipInY delay-1s").addClass("animated flipOutY");
+  $(".register").show();
+  $(".register").removeClass("flipOutY").addClass("animated flipInY delay-1s");
+});
+
+$("#login_link").click(function(){
+  $(".register").removeClass("flipInY delay-1s").addClass("flipOutY");
+  $(".login").show();
+  $(".login").removeClass("flipOutY").addClass("flipInY delay-1s");
 });
 
 
-});
+});//ready
