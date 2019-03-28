edff499ad8402ccefccdd27e079bbe231e22a1b3
praneethkumar900 <praneethkumar900@gmail.com>
2019-03-27 23:31:50 -0500

    Code Cleaning #1
    Indented and Formatted the files for better readbility of the code.

--- js/index.js
+++ js/index.js
@@ -1,83 +1,125 @@
-$(document).ready(function(){
-//animations on clicking register
-$("#register_link").click(function(){
-  $(".login").removeClass("flipInY delay-1s").addClass("animated flipOutY");
-  $(".register").show();
-  $(".register").removeClass("flipOutY").addClass("animated flipInY delay-1s");
-});
-//animations on clicking login_link buttton
-$("#login_link").click(function(){
-  $(".register").removeClass("flipInY delay-1s").addClass("flipOutY");
-  $(".login").show();
-  $(".login").removeClass("flipOutY").addClass("flipInY delay-1s");
-});
+$(document).ready(function() {
+/*-------------------- Animations on clicking register button --------------------*/
+    $("#register_link").click(function() {
+        $(".login").removeClass("flipInY delay-1s").addClass("animated flipOutY");
+        $(".register").show();
+        $(".register").removeClass("flipOutY").addClass("animated flipInY delay-1s");
+    });
 
-$("#name").parent().append('<tr><td><span class="error1">Name cannot be empty</span></td></tr>');
-$(".error1").addClass("error");
-//name validations
-$("#name").on('blur', function() {
-var name=$('#name').val();
-if(name==""){
- $(".error1").show();
-}else if(name!=""){
-//$("span").remove();
-$(".error1").hide();
-}
-});
+/*-------------------- Animations on clicking back_to_login buttton --------------------*/
+    $("#back_to_login").click(function() {
+        $(".register").removeClass("flipInY delay-1s").addClass("flipOutY");
+        $(".login").show();
+        $(".login").removeClass("flipOutY").addClass("flipInY delay-1s");
+    });
 
-//email validations
-$("#email").parent().append('<tr><td><span class="error2">Email cannot be empty</span></td></tr>');
-$(".error2").addClass("error");
-$("#email").on('blur', function() {
-var email=$("#email").val();
-if(email==""){
- $(".error2").show();
-}else if(email!=""){
-$(".error2").hide();
-}
-});
+    $("#name").parent().append('<tr><td><span class="error_name"></span></td></tr>');
+    $(".error_name").addClass("error");
 
-//username validations
-$("#rusername").parent().append('<tr><td><span class="error3">Username cannot be empty</span></td></tr>');
-$(".error3").addClass("error");
-$("#rusername").on('blur', function() {
-  var regex2 = /^^[a-zA-Z0-9]*$/i;
-  var username=$("#rusername").val();
-  var validUN= regex2.test(username);
-if(username==""){
- $(".error3").show();
-}
-else if(username!=""){
-    $(".error3").hide();
-  }
+/*-------------------- Name Validation --------------------*/
+    $("#name").on('blur', function() {
+        var regex_name = /^^[a-zA-Z]*$/i;
+        var name = $('#name').val();
+        var valid = regex_name.test(name);
+        if (name == "") {
+            $(".error_name").text("Name cannot be empty");
+            $(".error_name").show();
+        } else if (!valid) {
+            $(".error_name").text("Name cannot contain numbers");
+            $(".error_name").show();
+        } else {
+            $(".error_name").remove();
+            $(".error_name").hide();
+        }
+    });
 
-});
+/*-------------------- Email validation --------------------*/
+    $("#email").parent().append('<tr><td><span class="error_email"></span></td></tr>');
+    $(".error_email").addClass("error");
+    $("#email").on('blur', function() {
+        var regex_email = /^[\w-]+@([\w-]+\.)+[\w-]+$/i;
+        var email = $("#email").val();
+        var valid = regex_email.test(email);
+        if (email == "") {
+            $(".error_email").text("Email cannot be empty");
+            $(".error_email").show();
+        } else if (!valid) {
+            $(".error_email").text("Invalid email address");
+            $(".error_email").show();
+        } else {
+            $(".error_email").hide();
+        }
+    });
 
-//pwd validations
-$("#rpassword").parent().append('<tr><td><span class="error4">Password cannot be empty</span></td></tr>');
-$(".error4").addClass("error");
-$("#rpassword").on('blur', function() {
-var pwd=$("#rpassword").val();
-if(pwd==""){
- $(".error4").show();
-}else if(pwd!=""){
-$(".error4").hide();
-}
-});
+/*-------------------- Username Validation --------------------*/
+    $("#rusername").parent().append('<tr><td><span class="error_username">Username cannot be empty</span></td></tr>');
+    $(".error_username").addClass("error");
+    $("#rusername").on('blur', function() {
+        var username = $("#rusername").val();
+        if (username == "") {
+            $(".error_username").show();
+        } else if (username != "") {
+            $(".error_username").hide();
+        }
+    });
 
-//confirm pwd validations
-$("#cpassword").parent().append('<tr><td><span class="error5">Confirm Password cannot be empty</span></td></tr>');
-$(".error5").addClass("error");
-$("#cpassword").on('blur', function() {
-var cpwd=$("#cpassword").val();
-if(cpwd==""){
- $(".error5").show();
-}else if(cpwd!=""){
-$(".error5").hide();
-}
-});
+/*-------------------- Password Validation --------------------*/
+    $("#rpassword").parent().append('<tr><td><span class="error_rpwd">Password cannot be empty</span></td></tr>');
+    $(".error_rpwd").addClass("error");
+    $('#password-strength-meter').hide();
+    $("#rpassword").on('input', function() {
+        $(".error_rpwd").hide();
+        var pwd = $("#rpassword").val();
+        $('#password-strength-meter').show();
+        var meter = $('#password-strength-meter').val();
+        var strength = zxcvbn(pwd);
+        var value = strength.score;
+        $('#password-strength-meter').attr("value", value);
+        if (pwd != "") {
+            $('#password-strength-text').show().html('<p>' + strength.feedback.suggestions + '</p>').addClass("error");
+        }
+        if (pwd == "") {
+            $('#password-strength-meter').hide();
+            $('#password-strength-text').hide();
+        }
+    });
 
-$("#login_link").on('click',function(){
-$(".error").hide();$('.reg_input').val('');
-});
-});//ready
+    $("#rpassword").on('blur', function() {
+        var pwd = $("#rpassword").val();
+        if (pwd == "") {
+            $('#password-strength-meter').hide();
+            $('#password-strength-text').hide();
+            $(".error_rpwd").show();
+        } else if (pwd != "") {
+            $('#password-strength-meter').hide();
+            $(".error_rpwd").hide();
+        }
+    });
+
+/*-------------------- Confirm Password Validation --------------------*/
+    $("#cpassword").parent().append('<tr><td><span class="error_cpwd"></span></td></tr>');
+    $(".error_cpwd").addClass("error");
+    $("#cpassword").on('blur', function() {
+        var cpwd = $("#cpassword").val();
+        if (cpwd == "") {
+            $(".error_cpwd").text("Confirm Password cannot be empty");
+            $(".error_cpwd").show();
+        } else {
+            $(".error_cpwd").hide();
+        }
+    });
+    $("#cpassword").on('input', function() {
+        var pwd = $("#rpassword").val();
+        var cpwd = $("#cpassword").val();
+        if (cpwd != "" && cpwd != pwd) {
+            $(".error_cpwd").text("Passwords do not match");
+            $(".error_cpwd").show();
+        } else {
+            $(".error_cpwd").hide();
+        }
+    });
+    $("#back_to_login").on('click', function() {
+        $(".error").hide();
+        $('.reg_input').val('');
+    });
+}); //ready
