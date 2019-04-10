6c04da744b344f1080818f66bb0222e706014cdc
kpasagada <k.pasagada@gmail.com>
2019-04-10 03:03:46 -0500

    Changes to movies in Collection and code changes to main page

--- server/api/movies.js
+++ server/api/movies.js
@@ -1,28 +1,26 @@
-/* Customer routes file */
+/* Movie routes file */
 
 const express = require('express');
 const router = express.Router();
 const dbUtil = require('./../mongoDbUtil');
-const authUtil = require('./../authUtil');
-//const ObjectId = require('mongodb').ObjectId;
+const auth = require('./../authUtil');
 
 // Get movies with id endpoint
-router.post('/getmovies', function(request, response, next) {
-  var query={};
-  // query to display search results if search string is not null
-  if(request.body.search_str != null){
-     query={$text : { $search : request.body.search_str }};
-  }
-  else{
-    query={};
-  }
-dbUtil.getDb().collection("movies").find(query).toArray(function(error, result){
-  if (error) {
-      response.status(500).json({"error here": error.message});
-      return;
-  }
-  response.status(200).json(result);
-  });
+router.post('/getmovies', auth, function(request, response, next) {
+    var query = {};
+
+    // query to display search results if search string is not null
+    if(request.body.search_str != null){
+        query = { $text : { $search : request.body.search_str } };
+    }
+
+    dbUtil.getDb().collection("movies").find(query).toArray(function(error, result){
+        if (error) {
+            response.status(500).json({"error": error.message});
+            return;
+        }
+        response.status(200).json(result);
+    });
 });
 
 module.exports = router;
